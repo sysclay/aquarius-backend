ea92711e12b2b00c1e38a14eb727d19eee881774
sysclay <santi_25romero@hotmail.com>
2025-03-13 09:07:27 -0500

    update war

--- src/main/java/aquarius/crud/AquariusApplication.java
+++ src/main/java/aquarius/crud/AquariusApplication.java
@@ -10,5 +10,4 @@ public class AquariusApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(AquariusApplication.class, args);
 	}
-
 }
