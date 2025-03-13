6d208ccd5975f1d44872a31ebb18875fce3aa048
sysclay <santi_25romero@hotmail.com>
2025-03-13 09:00:03 -0500

    configuration para desplegar el war

--- src/main/java/aquarius/crud/AquariusApplication.java
+++ src/main/java/aquarius/crud/AquariusApplication.java
@@ -2,9 +2,10 @@ package aquarius.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AquariusApplication {
+public class AquariusApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AquariusApplication.class, args);
